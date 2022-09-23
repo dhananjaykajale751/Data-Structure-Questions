7bd5d58e5df1eaa265d1a33f25df401b015f49e0
dhananjaykajale751 <96171898+dhananjaykajale751@users.noreply.github.com>
2022-09-23 20:11:00 +0530

    Create SumArray.java

---
+++ SumArray.java
@@ -0,0 +1,17 @@
+//Sum of elements in ana array
+
+public class SumArray {
+   public static void Sum(int arr[]) {
+	   int sum=0;
+	   for(int i=0;i<arr.length;i++) {
+		   sum=sum+arr[i];
+	   }
+	   System.out.println(sum);
+   }
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6,7};
+		Sum(a);
+
+	}
+
+}
