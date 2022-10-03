676312bf0ca6c5ea4b31ac00bf538bc0c681d6c0
dhananjaykajale751 <96171898+dhananjaykajale751@users.noreply.github.com>
2022-10-04 00:34:20 +0530

    Update ReversingString.java

--- ReversingString.java
+++ ReversingString.java
@@ -1,1 +1,23 @@
+import java.util.*;
+public class Reverse {
+
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a word to reverse");
+		String inp=sc.next();
+		str.append(inp);
+		for(int i=0;i<str.length()/2;i++) {
+			 int front = i;
+			int  back = str.length()-i-1;
+			char frontchar = str.charAt(front);
+			char backchar = str.charAt(back);
+			str.setCharAt(front, backchar);
+			str.setCharAt(back, frontchar);
+			
+		}
+		System.out.println("The reversed word is "+str);
+	}
+
+}
 
